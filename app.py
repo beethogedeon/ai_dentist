896d0604dc4c2bd0f58bab25ad58acd9a3cde740
beethogedeon <beethovengedeon@gmail.com>
2023-08-03 01:28:47 +0100

    v1.0

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from streamlit_chat import message
+from utils import generate_response
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
