8e116ed94309467962ae4a4f08371b3b061a1b3e
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-04-29 17:50:11 +0100

    Upgrade model to gpt-4

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def generate_response(prompt):
     st.session_state['messages'].append({"role": "user", "content": prompt})
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-turbo",
         messages=st.session_state['messages']
     )
     response = completion.choices[0].message.content
